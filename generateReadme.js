fd2c61f775c4cb624156c74235b2d2e092cc26e3
寻觅～流光 <oncwnuNXqZnUU7jxiXCxX_ATjPJo@git.weixin.qq.com>
2025-04-30 21:37:29 +0800

    add 2025-04-30

--- generateReadme.js
+++ generateReadme.js
@@ -1,38 +1,45 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 const rootDir = ".";
 const outputPath = path.join(rootDir, "README.md");
 
 function generateTree(dirPath, indent = "", isLast = false) {
-  let tree = "";
-  const items = fs.readdirSync(dirPath).sort();
+    let tree = "";
+    const items = fs.readdirSync(dirPath).sort();
 
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    // 添加这行来忽略.git目录
-    if (item === ".git") continue;
-    const fullPath = path.join(dirPath, item);
-    const stat = fs.statSync(fullPath);
-    const isLastItem = i === items.length - 1;
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        // 添加这行来忽略.git目录
+        if (item === ".git") continue;
+        if (item === ".gitignore") continue;
+        if (item === "package.json") continue;
+        if (item === "node_modules") continue;
+        if (item === ".prettierrc.json") continue;
+        if (item === "generateReadme.js") continue;
+        if (item === "package-lock.json") continue;
+        if (item === "commit-with-date.js") continue;
+        const fullPath = path.join(dirPath, item);
+        const stat = fs.statSync(fullPath);
+        const isLastItem = i === items.length - 1;
 
-    if (stat.isDirectory()) {
-      tree += `${indent}${isLast ? "    " : "│   "}`;
-      tree += isLastItem ? "└── " : "├── ";
-      tree += `${item}/\n`;
-      tree += generateTree(
-        fullPath,
-        indent + (isLast ? "    " : "│   "),
-        isLastItem
-      );
+        if (stat.isDirectory()) {
+            tree += `${indent}${isLast ? "    " : "│   "}`;
+            tree += isLastItem ? "└── " : "├── ";
+            tree += `${item}/\n`;
+            tree += generateTree(
+                fullPath,
+                indent + (isLast ? "    " : "│   "),
+                isLastItem
+            );
+        }
     }
-  }
 
-  return tree;
+    return tree;
 }
 
 const mdContent = `# LEETCODE-SOLUTIONS\n\n\`\`\`\n${rootDir}/\n${generateTree(
-  rootDir
+    rootDir
 )}\`\`\``;
 
 fs.writeFileSync(outputPath, mdContent, "utf-8");
