abe5cc1eed045bf8c157be2b821425683a53004a
寻觅～流光 <oncwnuNXqZnUU7jxiXCxX_ATjPJo@git.weixin.qq.com>
2025-05-01 15:12:41 +0800

    add 2025-05-01

--- 二叉树/199. 二叉树的右视图/二叉树的右视图.js
+++ 二叉树/199. 二叉树的右视图/二叉树的右视图.js
@@ -11,17 +11,42 @@
  * @return {number[]}
  */
 var rightSideView = function (root) {
-    // 采用层序遍历来解决这个题目
+    // // 采用层序遍历来解决这个题目
+    // let ans = [];
+    // if (root === null) {
+    //     return [];
+    // }
+    // let queue_arr = [root];
+    // while (queue_arr.length > 0) {
+    //     let length = queue_arr.length;
+    //     let temp_queue = [];
+    //     for (let i = 0; i < length; i++) {
+    //         let temp_node = queue_arr.shift();
+    //         if (i === length - 1) {
+    //             ans.push(temp_node.val);
+    //         }
+    //         if (temp_node.left !== null) {
+    //             temp_queue.push(temp_node.left);
+    //         }
+    //         if (temp_node.right !== null) {
+    //             temp_queue.push(temp_node.right);
+    //         }
+    //     }
+    //     queue_arr = temp_queue;
+    // }
+    // return ans;
+
     let ans = [];
-    if (root === null) {
-        return [];
-    }
-    const queue_arr = [root];
-    ans.push(root.val);
-
-    while (queue_arr.length > 0) {
-        let length = queue_arr.length;
-
-        for (let i = 0; i < length; i++) {}
-    }
+    const get_height_right = (root, height) => {
+        if (root === null) {
+            return 0;
+        }
+        if (height === ans.length) {
+            ans.push(root.val);
+        }
+        get_height_right(root.right, height + 1);
+        get_height_right(root.left, height + 1);
+    };
+    get_height_right(root, 0);
+    return ans;
 };
